013e8fa244c81442a3a0254652190315c095fc26
verocortesferrera <verocortesferrera@gmail.com>
2017-12-13 17:23:16 -0300

    splash y botonera inicial

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+/*splash*/
+	setTimeout(function() {
+		$(".content").fadeOut(500);
+		},3000);
+		setTimeout(function() {
+			$(".content2").fadeIn(500);
+		});
+});
