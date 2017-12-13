58264b9112b3d415c5c08c2ad290cec7f727571d
verocortesferrera <verocortesferrera@gmail.com>
2017-12-13 19:13:50 -0300

    modal sing up

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,3 +7,39 @@ $(document).ready(function() {
 			$(".content2").fadeIn(500);
 		});
 });
+
+$("#formulario").submit(function () {  
+    if(isNaN($("#telefono").val())) {  
+        alert("El teléfono solo debe contener números");  
+        return false;  
+    }  
+    return false;  
+});  
+
+$('#number').keyup(function(){
+	var number = $('#number').val();
+	number.val('');
+	if(number==10)
+	function code() {
+  var code = "";
+  /*var str = "123456789";*/
+  for (var i = 0; i < 3; i++) {
+  	code += number.charAt(Math.floor(Math.random() * str.length));
+  }
+  return code;
+}
+
+alert(code());
+	
+});
+
+function code() {
+  var code = "";
+  /*var str = "123456789";*/
+  for (var i = 0; i < 3; i++) {
+  	code += number.charAt(Math.floor(Math.random() * str.length));
+  }
+  return code;
+}
+
+alert(code());
